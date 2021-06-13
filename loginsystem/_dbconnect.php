c9b66e66515b1f60a8c662235b9e7aa15196bf30
kundan <kundanpremnayak1234@gmail.com>
2021-06-13 17:46:39 +0530

    first commit

---
+++ loginsystem/_dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "todolist";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+// if (!$conn){
+//     die("Error". mysqli_connect_error());
+// }
+// else{
+//    echo "success ";
+// }
+
+?>
